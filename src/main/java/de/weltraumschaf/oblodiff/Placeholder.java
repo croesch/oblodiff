8d784ca42063d456acd22931509eda78c2df6fc6
Sven Strittmatter <sven.strittmatter@iteratec.de>
2015-11-17 22:09:05 +0100

    Initial commit.

---
+++ src/main/java/de/weltraumschaf/oblodiff/Placeholder.java
@@ -0,0 +1,8 @@
+package de.weltraumschaf.oblodiff;
+
+/**
+ * This class is only a place holder that git commits the directory.
+ */
+public final class Placeholder {
+
+}
---
+++ src/site/markdown/index.md
@@ -0,0 +1,3 @@
+# ObloDiff
+
+TODO
