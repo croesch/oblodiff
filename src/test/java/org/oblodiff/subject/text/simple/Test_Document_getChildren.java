0766727b6a7fe4b2063b1cab03219ef03b5b1008
Christian Rösch <christianroesch@gmx.net>
2015-11-19 21:09:41 +0100

    Remove unnecessary FQN.

--- src/test/java/org/oblodiff/subject/text/simple/Test_Document_getChildren.java
+++ src/test/java/org/oblodiff/subject/text/simple/Test_Document_getChildren.java
@@ -10,32 +10,32 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class Test_Document_getChildren {
   @Test
   public void should_Be_Empty_If_Document_Is_Empty() {
-    assertThat(new org.oblodiff.subject.text.linebased.Document("").getChildren()).isEmpty();
+    assertThat(new Document("").getChildren()).isEmpty();
   }
 
   @Test
   public void should_Return_One_Child_If_Consists_Of_One_CarriageReturn() {
-    assertThat(new org.oblodiff.subject.text.linebased.Document("\r").getChildren()).containsExactly(new Character('\r'));
+    assertThat(new Document("\r").getChildren()).containsExactly(new Character('\r'));
   }
 
   @Test
   public void should_Return_One_Child_If_Consists_Of_One_LineFeed() {
-    assertThat(new org.oblodiff.subject.text.linebased.Document("\n").getChildren()).containsExactly(new Character('\n'));
+    assertThat(new Document("\n").getChildren()).containsExactly(new Character('\n'));
   }
 
   @Test
   public void should_Return_One_Child_If_Consists_Of_One_Line() {
-    assertThat(new org.oblodiff.subject.text.linebased.Document("super").getChildren()).containsExactly(new Line("super"));
+    assertThat(new Document("super").getChildren()).containsExactly(new Line("super"));
   }
 
   @Test
   public void should_Add_NewLine_As_Character() {
-    assertThat(new org.oblodiff.subject.text.linebased.Document("super\n").getChildren()).containsExactly(new Line("super"), new Character('\n'));
+    assertThat(new Document("super\n").getChildren()).containsExactly(new Line("super"), new Character('\n'));
   }
 
   @Test
   public void should_Add_CarriageReturn_As_Character() {
-    assertThat(new org.oblodiff.subject.text.linebased.Document("super\r").getChildren()).containsExactly(new Line("super"), new Character('\r'));
+    assertThat(new Document("super\r").getChildren()).containsExactly(new Line("super"), new Character('\r'));
   }
 
   @Test
