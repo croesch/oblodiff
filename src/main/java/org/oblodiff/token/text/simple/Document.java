baa25d506a7d73ff75f4aaf257dcbaec98ccf603
Sven Strittmatter <sven.strittmatter@iteratec.de>
2015-11-23 21:26:21 +0100

    Fix the javadoc and add missing finals.

--- src/main/java/org/oblodiff/token/text/simple/Document.java
+++ src/main/java/org/oblodiff/token/text/simple/Document.java
@@ -4,12 +4,16 @@ import java.util.Collection;
 import org.oblodiff.token.api.Token;
 import org.oblodiff.token.text.TextualSplittableToken;
 
-import java.util.List;
 import java.util.Stack;
 
 /**
- * This is a document is a full text that is simple in terms of just plain text. It consists of paragraphs, sentences,
- * words and characters.
+ * This is a document which is a full text that is simple in terms of just plain text.
+ * <p>
+ * It consists of paragraphs, sentences, words and characters.
+ * </p>
+ *
+ * XXX I think it is not a good idea to have two Document classes (line based). This makes it impossible for API clients
+ * to use both of them, unless they reference it full qualified.
  *
  * @since 1.0.0
  * @author Christian Rösch &lt;christianroesch@gmx.net&gt;
@@ -19,18 +23,24 @@ public class Document extends TextualSplittableToken {
     private static final Character CARRIAGE_RETURN = '\r';
     private static final Character LINE_FEED = '\n';
 
-    public Document(String s) {
-        super(s);
+    /**
+     * Dedicated constructor.
+     *
+     * @param content must not be {@code null}
+     */
+    public Document(final String content) {
+        super(content);
     }
 
     @Override
-    protected boolean shouldSplitAt(int i, Character character) {
+    protected boolean shouldSplitAt(final int i, final Character character) {
         final boolean isCarriageReturn = character.equals(CARRIAGE_RETURN);
+
         return (isCarriageReturn || character.equals(LINE_FEED)) && containsBreakTillNextNonWhitespace(i + 1,
             isCarriageReturn);
     }
 
-    private boolean containsBreakTillNextNonWhitespace(int begin, boolean isCarriageReturn) {
+    private boolean containsBreakTillNextNonWhitespace(final int begin, final boolean isCarriageReturn) {
         for (int i = begin; i < getContent().length(); ++i) {
             final Character character = getContent().charAt(i);
 
@@ -46,16 +56,17 @@ public class Document extends TextualSplittableToken {
                 return false;
             }
         }
+
         return false;
     }
 
     @Override
-    protected Token newToken(String content) {
+    protected Token newToken(final String content) {
         return new Sentence(content);
     }
 
     @Override
-    protected void endReached(Collection<Token> children, int begin, Character cc) {
+    protected void endReached(final Collection<Token> children, final int begin, Character cc) {
         final Stack<Character> whitespaces = new Stack<>();
 
         for (int i = getContent().length() - 1; i >= begin; --i) {
@@ -67,15 +78,18 @@ public class Document extends TextualSplittableToken {
                 break;
             }
         }
+
         addToken(children, begin, getContent().length() - whitespaces.size());
+
         while (!whitespaces.isEmpty()) {
             children.add(new org.oblodiff.token.text.Character(whitespaces.pop()));
         }
     }
 
     @Override
-    protected int addDivider(Collection<Token> children, int begin, Character cc) {
+    protected int addDivider(final Collection<Token> children, final int begin, Character cc) {
         int inserted = 0;
+
         for (int i = begin; i < getContent().length(); ++i) {
             final Character character = getContent().charAt(i);
 
