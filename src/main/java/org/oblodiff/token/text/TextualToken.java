7ba5fcfbec06c6324f91d642b59968636321d1fb
Sven Strittmatter <sven.strittmatter@iteratec.de>
2015-11-23 19:56:22 +0100

    Add XXX marker to class.

--- src/main/java/org/oblodiff/token/text/TextualToken.java
+++ src/main/java/org/oblodiff/token/text/TextualToken.java
@@ -5,13 +5,19 @@ import org.oblodiff.token.BasicToken;
 /**
  * A textual token is a token representing a text.
  *
+ * XXX For what is this class good? It does not add any behavior/property to BasicToken.
  * @since 1.0.0
  * @param <T> the type of the content this token represents
  * @author Christian Rösch &lt;christianroesch@gmx.net&gt;
  */
 public abstract class TextualToken<T> extends BasicToken<T> {
 
-    public TextualToken(T ct) {
-        super(ct);
+    /**
+     * Dedicated constructor.
+     *
+     * @param content must not be {@code null}
+     */
+    public TextualToken(final T content) {
+        super(content);
     }
 }
